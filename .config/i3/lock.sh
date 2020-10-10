207f3434f8ef7eb61162632c0c1ea7a51f9c779f
vntasis <vasilisntasis94@gmail.com>
2020-10-10 18:28:22 +0200

    Add i3 related dotfiles

---
+++ .config/i3/lock.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+scrot -o /tmp/screen.png
+convert /tmp/screen.png -paint 3 /tmp/screen.png
+[[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png ~/.config/i3/lock.png -gravity center  -composite -matte /tmp/screen.png
+
+i3lock -i /tmp/screen.png
